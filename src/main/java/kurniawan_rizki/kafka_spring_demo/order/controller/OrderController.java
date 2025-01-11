ec09d8d94ac119577af7fcf5f3a0de013d15ee5b
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2025-01-11 16:47:21 +0700

    feat(order) add: order controller:

--- src/main/java/kurniawan_rizki/kafka_spring_demo/order/controller/OrderController.java
+++ src/main/java/kurniawan_rizki/kafka_spring_demo/order/controller/OrderController.java
@@ -1,10 +1,26 @@
 package kurniawan_rizki.kafka_spring_demo.order.controller;
 
+import kurniawan_rizki.kafka_spring_demo.order.dto.OrderRequestDto;
+import kurniawan_rizki.kafka_spring_demo.order.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+     private final OrderService orderService;
+     public OrderController(OrderService orderService) {
+          this.orderService = orderService;
+     }
+
+     @PostMapping
+     public ResponseEntity<String> createOrder(@RequestBody OrderRequestDto req){
+          orderService.createOrder(req);
+          return ResponseEntity.ok("Order created successfully!");
+     }
+
 
 }
